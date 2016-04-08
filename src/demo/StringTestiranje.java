019620a9b87776b3749e3f642eca572a675bd37c
Dijana <deny.dijana@gmail.com>
2016-04-08 11:17:18 +0200

    Main klasa

---
+++ src/demo/StringTestiranje.java
@@ -0,0 +1,50 @@
+package demo;
+
+import java.util.Scanner;
+
+public class StringTestiranje {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Unesite Recenicu");
+		String recenica = input.nextLine();
+		
+		duzinaStringa(recenica);
+		parnePozicije(recenica);
+		neparnePozicije(recenica);
+		brojUppera(recenica);
+		brojLowecasa(recenica);
+		karakteri(recenica);
+		
+	}
+
+	private static void karakteri(String recenica) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void brojLowecasa(String recenica) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void brojUppera(String recenica) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void neparnePozicije(String recenica) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void parnePozicije(String recenica) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void duzinaStringa(String recenica) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
